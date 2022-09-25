0a2a8610ade554eae144e96e77278870f056597b
Brenda SC <brenda.saavedra@fondeadora.com>
2022-09-25 12:57:08 -0500

    feat: add functionality for transactions on blockchain

---
+++ backend/contracts/Transactions.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.15;
+
+contract Transactions {
+    // Number of transactions
+    uint256 transactionCount;
+
+    // This will be an event thar will be fired when a transaction is made
+    event Transfer(address from, address receiver, uint amount, string message, uint timestamp);
+
+    // Struct of the transactions thaat we will be storing on chain
+    struct Transaction {
+        address sender;
+        address receiver;
+        uint amount;
+        string message;
+        uint256 timestamp;
+    }
+
+    // Array of transactions
+    Transaction[] transactions;
+
+    // Function to add a transaction to the blockchain with our struct
+    function addToBlockchain(address payable receiver, uint amount, string memory message) public {
+        transactionCount += 1;
+        transactions.push(Transaction(message.sender, receiver, amount, message, block.timestamp));
+
+        // Emit the event we created earlier
+        emit Transfer(message.sender, receiver, amount, message, block.timestamp);
+    }
+
+    // Fucntion to get all the transactions
+    function getAllTransactions() public view returns (Transaction[] memory) {
+        return transactions;
+    }
+
+    // Function to get the number of transactions
+    function getTransactionCount() public view returns (uint256) {
+        return transactionCount;
+    }
+
+}
