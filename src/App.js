b5bc520bb799025fdbc3f7b17f5306451901fe4b
Brenda SC <brenda.saavedra@fondeadora.com>
2022-10-31 17:19:38 -0600

    feat: add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import styles from './styles/App.module.css'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className={styles.wrapper}>
+      <header>
+        <Navbar />
+      </header>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,29 @@
+import styles from '../styles/Navbar.module.css'
+import { ChevronDownIcon } from '@heroicons/react/24/solid'
+
 const Navbar = () => {
-  return <nav></nav>
+  return <nav className={styles.navigationContainer}>
+    <div className={styles.container}>
+      <div className={styles.logoContainer}>
+        <img 
+          src='../assets/venmo-logo.svg' 
+          alt='Venmo logo' 
+          className={styles.logoImage}/>
+      </div>
+      <div className={styles.actionsContainer}>
+        <p>
+          Hello, <span className={styles.accentColor}>UserAddress!</span> 👋
+        </p>
+        <ChevronDownIcon className={styles.arrowDownIcon} />
+        <div className={styles.avatarContainer}>
+          <img
+            className={styles.avatarImage}
+            src='https://i.pravatar.cc/40'
+            alt='' />
+        </div>
+      </div>
+    </div>
+  </nav>
 }
 
 export default Navbar
