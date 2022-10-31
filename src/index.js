1492bbad7ce0990f3469d04cc9da3b5083e7d1b3
Brenda SC <brenda.saavedra@fondeadora.com>
2022-10-31 17:42:04 -0600

    feat: wrap app in TransactionProvider

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { TransactionProvider } from './context/context'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <TransactionProvider>
+      <App />
+    </TransactionProvider>
   </React.StrictMode>
 );
 
