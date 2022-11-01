906d25defd9bceeea6df512f0f75815242f569df
Brenda SC <brenda.saavedra@fondeadora.com>
2022-10-31 19:17:53 -0600

    feat: initial activity card

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import styles from './styles/App.module.css'
 import Navbar from './components/Navbar'
+import ActivityCard from "./components/activity/ActivityCard"
+import TransactionForm from './components/transaction/TransactionForm'
 
 function App() {
   return (
@@ -7,6 +9,14 @@ function App() {
       <header>
         <Navbar />
       </header>
+      <main className={styles.mainContainer}>
+        <div className={styles.activityContainer}>
+          <ActivityCard />
+        </div>
+        <div className={styles.sideContainer}>
+          <TransactionForm />
+        </div>
+      </main>
     </div>
   );
 }
--- src/components/activity/ActivityCard.jsx
+++ src/components/activity/ActivityCard.jsx
@@ -1,5 +1,56 @@
+import { ChatBubbleBottomCenterIcon, GlobeAltIcon, HeartIcon } from "@heroicons/react/24/outline";
+import { UserGroupIcon, UserIcon } from "@heroicons/react/24/solid";
+import styles from "../../styles/Activity.module.css";
+
 function ActivityCard() {
-  return <div></div>
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.tabContainer}>
+        <p className={styles.tabTitle}>Activity</p>
+        <div className={styles.navigationContainer}>
+          <div className={styles.navigationItem} data-current>
+            <GlobeAltIcon className={styles.navigationIcon} />
+          </div>
+          <div className={styles.navigationItem}>
+            <UserIcon className={styles.navigationIcon} />
+          </div>
+          <div className={styles.navigationItem}>
+            <UserGroupIcon className={styles.navigationIcon} />
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.feedList}>
+            <div key='' className={styles.feedItem}>
+              <div className={styles.avatarContainer}>
+                <img
+                  className={styles.avatarImage}
+                  src='https://i.pravatar.cc/40'
+                  alt=""
+                />
+              </div>
+
+              <div className={styles.feedDetails}>
+                <h3 className={styles.feedAuthor}>
+                ADDRESS1 to ADDRESS2
+                </h3>
+                <span className={styles.feedCreatedAt}>
+                  Date
+                  <GlobeAltIcon className={styles.globeIcon} />
+                </span>
+
+                <p className={styles.feedBody}>Message</p>
+              </div>
+
+              <div className={styles.feedCta}>
+                <HeartIcon className={styles.likeIcon} />
+                <ChatBubbleBottomCenterIcon className={styles.commentIcon} />
+              </div>
+            </div>
+      </div>
+    </div>
+  );
 }
 
-export default ActivityCard
+export default ActivityCard;
