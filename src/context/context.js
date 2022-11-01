d325de46d26f3937e8486a2a6c5b435e71867b18
Brenda SC <brenda.saavedra@fondeadora.com>
2022-10-31 19:45:32 -0600

    feat: add contract constants (ABI)

--- src/context/context.js
+++ src/context/context.js
@@ -1,12 +1,32 @@
 import { useEffect, useState, createContext } from "react";
 import { ethers } from 'ethers'
+import { contractABI, contractAddress } from '../utils/constants'
 
 export const TransactionContext = createContext()
 
 const { ethereum } = window
 
+const createEthereumContract = () => {
+    const provider = new ethers.providers.Web3Provider(ethereum)
+    const signer = provider.getSigner()
+    const transactionsContract = new ethers.Contract(
+      contractAddress,
+      contractABI,
+      signer,
+    )
+  
+    return transactionsContract
+}
+
 export const TransactionProvider = ({ children }) => {
     const [currentAccount, setCurrentAccount] = useState('')
+    const [addressTo, setAddressTo] = useState('')
+    const [amount, setAmount] = useState(0)
+    const [message, setMessage] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
+    const [transactionCount, setTransactionCount] = useState(
+      localStorage.getItem('transactionCount'),
+    )
 
     useEffect(() => {
         checkIfWalletIsConnected()
@@ -41,6 +61,22 @@ export const TransactionProvider = ({ children }) => {
             throw new Error('No ethereum object')
         }
     } 
+
+    const checkIfTransactionsExists = async () => {
+        try {
+          if (ethereum) {
+            const transactionsContract = createEthereumContract()
+            const currentTransactionCount =
+              await transactionsContract.getTransactionCount()
+    
+            window.localStorage.setItem('transactionCount', currentTransactionCount)
+          }
+        } catch (error) {
+          console.log(error)
+    
+          throw new Error('No ethereum object')
+        }
+      }
 
     return (
         <TransactionContext.Provider value={{ connectWallet, currentAccount }}>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,4 @@
-const contractAddress = '0x33098FEE3382ECA803F2bBF113a83caca73dfC7c'
+import abi from './Transactions.json'
+
+export const contractAddress = '0x33098FEE3382ECA803F2bBF113a83caca73dfC7c'
+export const contractABI = abi.abi
